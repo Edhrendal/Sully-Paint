6f99c6d0412debb54eb29a9297ba2dc24a0c3179
Valentin Jouanigot <valentin.jouanigot@sully-group.fr>
2017-03-03 14:47:45 +0100

    Feat(Paint): Enregistrement et restauration de l'état du canvas

--- src/app.js
+++ src/app.js
@@ -1,52 +1,57 @@
-'use strict'
+'use strict';
 
-const http = require('http')
-const path = require('path')
-const express = require('express')
-const escape = require('escape-html')
-let io = require('socket.io')
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const escape = require('escape-html');
+let io = require('socket.io');
 
-const port = 3000
-let app = express()
-app.use('/static', express.static(path.join(__dirname, './public')))
-app.set('views', path.join('./src', './views'))
-app.set('view engine', 'pug')
+const port = 3000;
+let app = express();
+app.use('/static', express.static(path.join(__dirname, './public')));
+app.set('views', path.join('./src', './views'));
+app.set('view engine', 'pug');
 
-let server = http.Server(app)
-io = io.listen(server)
+let server = http.Server(app);
+io = io.listen(server);
+
+// STORES
+
+var drawingStore = [];
 
 // ROUTING
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Authentification'
   })
-})
+});
 
 app.get('/paint', (req, res) => {
   res.render('paint', {
     title: 'Détente'
   })
-})
+});
 
 // SOCKET
 io.sockets.on('connection', (socket) => {
   // Connection
   socket.on('connectionToChannel', author => {
-    console.log(`${author} is connected.`)
+    console.log(`${author} is connected.`);
     io.sockets.emit('connectedToChannel', author.trim())
-  })
+    io.sockets.emit('restoreDraw', drawingStore);
+  });
 
   // Chatbox
   socket.on('send', data => {
-    let message = escape(data.message).trim()
-    let author = escape(data.author).trim()
+    let message = escape(data.message).trim();
+    let author = escape(data.author).trim();
     io.sockets.emit('write', { message, author })
-  })
+  });
 
   // Paint
   socket.on('drawing', data => {
-    let last = socket.lastPoint || data
-    io.sockets.emit('draw', {
+    let last = socket.lastPoint || data;
+    let draw = {
       last,
       new: {
         x: data.x,
@@ -54,17 +59,19 @@ io.sockets.on('connection', (socket) => {
       },
       color: data.color,
       width: data.width
-    })
+    };
+    io.sockets.emit('draw', draw);
+    drawingStore.push(draw);
 
     socket.lastPoint = {
       x: data.x,
       y: data.y
     }
-  })
+  });
 
   socket.on('stopdrawing', _ => {
     delete socket.lastPoint
   })
-})
+});
 
-server.listen(port)
+server.listen(port);
--- src/public/js/paint.js
+++ src/public/js/paint.js
@@ -1,99 +1,117 @@
-const socket = io.connect('192.168.44.88:3000')
-const canvas = document.querySelector('#canvas')
-const board = canvas.getContext('2d')
-const inputColorBrush = document.querySelector('#colorBrush')
-const inputWidthBrush = document.querySelector('#widthBrush')
+const socket = io.connect('localhost:3000');
+const canvas = document.querySelector('#canvas');
+const board = canvas.getContext('2d');
+const inputColorBrush = document.querySelector('#colorBrush');
+const inputWidthBrush = document.querySelector('#widthBrush');
 
 // Settings
-board.lineJoin = 'round'
-board.lineCap = 'round'
-board.strokeStyle = inputColorBrush.value
-board.lineWidth = inputWidthBrush.value
+board.lineJoin = 'round';
+board.lineCap = 'round';
+board.strokeStyle = inputColorBrush.value;
+board.lineWidth = inputWidthBrush.value;
 
 function onmousemove (event) {
   socket.emit('drawing', {x: event.offsetX, y: event.offsetY, color: board.strokeStyle, width: board.lineWidth})
 }
 canvas.addEventListener('mousedown', function (event) {
   canvas.addEventListener('mousemove', onmousemove)
-})
+});
 canvas.addEventListener('mouseup', function (event) {
-  socket.emit('stopdrawing')
+  socket.emit('stopdrawing');
   canvas.removeEventListener('mousemove', onmousemove)
-})
+});
 canvas.addEventListener('mouseleave', function (event) {
-  socket.emit('stopdrawing')
+  socket.emit('stopdrawing');
   canvas.removeEventListener('mousemove', onmousemove)
-})
+});
 
 // dessin
 socket.on('draw', function (data) {
-  let color = board.strokeStyle
-  let width = board.lineWidth
+  let color = board.strokeStyle;
+  let width = board.lineWidth;
 
-  board.strokeStyle = data.color
-  board.lineWidth = data.width
-  board.beginPath()
-  board.moveTo(data.last.x, data.last.y)
-  board.lineTo(data.new.x, data.new.y)
-  board.stroke()
+  board.strokeStyle = data.color;
+  board.lineWidth = data.width;
+  board.beginPath();
+  board.moveTo(data.last.x, data.last.y);
+  board.lineTo(data.new.x, data.new.y);
+  board.stroke();
 
-  board.strokeStyle = color
+  board.strokeStyle = color;
   board.lineWidth = width
-})
+});
+
+// dessin
+socket.on('restoreDraw', function (drawingStore) {
+  drawingStore.forEach(function(data) {
+    let color = board.strokeStyle;
+    let width = board.lineWidth;
+
+    board.strokeStyle = data.color;
+    board.lineWidth = data.width;
+    board.beginPath();
+    board.moveTo(data.last.x, data.last.y);
+    board.lineTo(data.new.x, data.new.y);
+    board.stroke();
+
+    board.strokeStyle = color;
+    board.lineWidth = width
+  });
+});
 
 // Color selection
 inputColorBrush.addEventListener('change', function () {
-  canvas.removeEventListener('mousemove', onmousemove)
+  canvas.removeEventListener('mousemove', onmousemove);
   board.strokeStyle = inputColorBrush.value
-})
+});
 
 // Width selection
 inputWidthBrush.addEventListener('change', function () {
-  canvas.removeEventListener('mousemove', onmousemove)
+  canvas.removeEventListener('mousemove', onmousemove);
   board.lineWidth = inputWidthBrush.value
-})
+});
 
 // CHATBOX
-let author = getCookie('login')
-socket.emit('connectionToChannel', author)
+let author = getCookie('login');
+socket.emit('connectionToChannel', author);
 
-let chatbox = document.querySelector('#chatbox')
-const formChatbox = document.querySelector('#sendMessage')
-const inputChatbox = document.querySelector('#message')
+let chatbox = document.querySelector('#chatbox');
+const formChatbox = document.querySelector('#sendMessage');
+const inputChatbox = document.querySelector('#message');
 
 formChatbox.addEventListener('submit', (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
-  let message = inputChatbox.value
-  inputChatbox.value = ''
+  let message = inputChatbox.value;
+  inputChatbox.value = '';
   if (message.trim() !== '') {
     socket.emit('send', { author, message })
   }
-})
+});
 
 socket.on('connectedToChannel', (author) => {
-  let date = new Date()
-  date = `${date.toDateString()} - ${date.getHours()}:${date.getMinutes()}`
+  let date = new Date();
+  date = `${date.toDateString()} - ${date.getHours()}:${date.getMinutes()}`;
   chatbox.innerHTML =
     `${chatbox.innerHTML}
         <div class="connection label label-success">
             ${author} a rejoint le chat le ${date}
         </div>`
-})
+});
 
 socket.on('write', (data) => {
   if (chatbox.scrollTop === chatbox.scrollHeight) {
     console.log('On descend !')
   }
-  let date = new Date()
+  let date = new Date();
   // Format of time
-  let hours = date.getHours()
-  hours = hours < 10 ? `0${hours}` : hours
-  let minutes = date.getMinutes()
-  minutes = minutes < 10 ? `0${minutes}` : minutes
-  let seconds = date.getSeconds()
-  seconds = seconds < 10 ? `0${seconds}` : seconds
-  date = `[${hours}:${minutes}:${seconds}]`
+  let hours = date.getHours();
+  hours = hours < 10 ? `0${hours}` : hours;
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  let seconds = date.getSeconds();
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+  date = `[${hours}:${minutes}:${seconds}]`;
   // Display
   chatbox.innerHTML =
     `${chatbox.innerHTML}
@@ -101,8 +119,8 @@ socket.on('write', (data) => {
             <span class="date">${date}</span>
             <span class="author">${data.author} : </span>
             ${data.message}
-        </div>`
+        </div>`;
   // Scroll bar bottom position
 
   chatbox.scrollTop = chatbox.scrollHeight
-})
+});
