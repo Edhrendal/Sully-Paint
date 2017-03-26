27a137d4e45a3b07b8fed71336b3aabb1f67ea83
Edhrendal <bvillena.git@laposte.net>
2017-03-26 22:55:37 +0200

    Use session instead of cookies
    * Session is managing with res.session object.
      -> Add a user object with login.
      -> Login is got by login form.
      -> Login is passed to paint view and used by variable "author".
    * Add a middleware to redirect user if not logged.
      -> Redirect to paint if user is on login page but logged.
    
    TODO Add disconnection action.

--- src/app.js
+++ src/app.js
@@ -4,17 +4,32 @@ const http = require('http')
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const fs = require('fs')
 const escape = require('escape-html')
 let io = require('socket.io')
 
 const connectionFile = JSON.parse(fs.readFileSync(path.join(__dirname, './public/json/connection.json'), 'utf8'))
 const port = connectionFile.port
+const loginPath = '/'
 // Application
 let app = express()
 // -> POST - BodyParser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+// -> Session
+app.use(session({
+  secret: 'paint',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use((req, res, next) => {
+  let user = req.session.user
+  if (!user) {
+    req.session.user = {}
+  }
+  next()
+})
 // -> Views parameters
 app.use('/static', express.static(path.join(__dirname, './public')))
 app.set('views', path.join('./src', './views'))
@@ -23,24 +38,52 @@ app.set('view engine', 'pug')
 let server = http.Server(app)
 io = io.listen(server)
 
+// MiddleWares
+// -> Redirect to Login Page not logged users but to Paint for logged users
+let loginRedirection = (req, res, next) => {
+  let user = req.session.user
+  let url = req.url
+  let redirectToLoginPage = true
+
+  // User is logged ; don't need to redirect to login page
+  if (typeof user !== 'undefined' && typeof user.login !== 'undefined' && user.login.trim() !== '') {
+    redirectToLoginPage = false
+  }
+
+  // Redirection
+  if (redirectToLoginPage) {
+    if (url !== loginPath) {
+      return res.redirect(loginPath)
+    }
+  } else { // Logged user
+    // Don't need to stay on login Page if logged
+    if (url === loginPath) {
+      return res.redirect('/paint')
+    }
+  }
+
+  next()
+}
+
 // STORES
 let drawingStore = []
 
 // ROUTING
-app.get('/', (req, res) => {
+app.get(loginPath, [loginRedirection], (req, res) => {
   res.render('index', {
     title: 'Authentification'
   })
 })
 
-app.post('/', (req, res) => {
-  let login = request.body.login
+app.post(loginPath, (req, res) => {
+  req.session.user.login = req.body.login
   res.redirect('/paint')
 })
 
-app.get('/paint', (req, res) => {
+app.get('/paint', [loginRedirection], (req, res) => {
   res.render('paint', {
-    title: 'Détente'
+    title: 'Détente',
+    login: req.session.user.login
   })
 })
 
@@ -94,5 +137,5 @@ io.sockets.on('connection', (socket) => {
 })
 
 server.listen(port, _ => {
-  console.log(`Server is listening to port ${port}`)
+  console.log(`Server is listening to port ${port}.`)
 })
